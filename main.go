2a426910f9dd8bac60a94cd90a5bb4690d41ddb3
Luken <github@luken.cc>
2023-01-29 13:50:28 +0100

    :construction: wip additional logging for main flow

--- main.go
+++ main.go
@@ -22,12 +22,17 @@ func main() {
 
 	sc := make(chan os.Signal, 1)
 	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
-	lastTick := time.Now()
 
 	client = tesla.NewClient(log)
 	car = client.UseMainVehicle()
 
 	log.Infow("Started")
+
+	//tmp := car.GetVehicleState()
+	//
+	//if tmp != nil {
+	//	log.Infow("TEMP", "tmp", tmp)
+	//}
 
 	for {
 		select {
@@ -37,18 +42,26 @@ func main() {
 		default:
 		}
 
-		if time.Now().Sub(lastTick).Seconds() < 30 {
-			time.Sleep(1 * time.Second)
-			continue
-		}
-
-		lastTick = time.Now()
 		tick()
+		time.Sleep(1 * time.Second)
 	}
 }
 
+var (
+	lastTick = time.Now()
+	sleeping = false
+	nextLock = false
+)
+
 func tick() {
-	log.Debug("Tick")
+	elapsed := time.Now().Sub(lastTick).Seconds()
+
+	if elapsed < 10 || (sleeping && elapsed < 60) {
+		return
+	}
+
+	lastTick = time.Now()
+	log.Debugw("Tick", "elapsed", elapsed)
 
 	info := car.GetInfo()
 
@@ -57,16 +70,71 @@ func tick() {
 		return
 	}
 
-	if info.Response.State == "asleep" {
-		log.Debug("car is sleeping")
+	sleeping = info.Response.State != "online"
+
+	if sleeping {
+		log.Debugw("car is sleeping", "state", info.Response.State)
 		return
 	}
 
-	log.Infow("VehicleInfo", "State", info.Response.State)
+	car.WakeUp() // keep the car online
+	driveState := car.GetDriveState()
 
-	//wakeUp := car.WakeUp()
+	if driveState == nil {
+		log.Error("drive state unavailable")
+		return
+	}
+
+	log.Infow("DriveState",
+		"Shift", driveState.Response.ShiftState,
+		"Speed", driveState.Response.Speed,
+	)
+
+	vehicleState := car.GetVehicleState()
+
+	if vehicleState == nil {
+		log.Error("vehicle state unavailable")
+		return
+	}
+
+	log.Infow("VehicleState",
+		"IsUserPresent", vehicleState.Response.IsUserPresent,
+		"DisplayState", vehicleState.Response.CenterDisplayState,
+		"Locked", vehicleState.Response.Locked,
+		"FrontLeft", vehicleState.Response.DoorDriverFront,
+		"FrontRight", vehicleState.Response.DoorPassengerFront,
+		"BackLeft", vehicleState.Response.DoorDriverRear,
+		"BackRight", vehicleState.Response.DoorPassengerRear,
+		"FrontTrunk", vehicleState.Response.DoorFrontTrunk,
+		"RearTrunk", vehicleState.Response.DoorRearTrunk,
+	)
+
+	shouldLock := !vehicleState.Response.Locked && !vehicleState.Response.IsUserPresent
+
+	if !shouldLock {
+		nextLock = false
+		log.Debug("shouldLock - requirements not met")
+		return
+	}
+
+	if !nextLock {
+		nextLock = true
+		log.Debug("shouldLock - locking doors on next iteration")
+		return
+	}
+
+	//lockDoorsResult := car.LockDoors()
 	//
-	//if wakeUp != nil {
-	//	log.Infow("WakeUp", "Response", wakeUp.Response)
+	//if lockDoorsResult == nil {
+	//	log.Error("failed to lock doors")
+	//	return
 	//}
+	//
+	//if !lockDoorsResult.Result {
+	//	log.Debugw("unable to lock doors", "result", lockDoorsResult)
+	//	return
+	//}
+
+	log.Info("Locking Doors")
+	nextLock = false
 }
